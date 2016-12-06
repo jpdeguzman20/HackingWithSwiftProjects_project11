b7903c47dd755a97a2765d13376464a9fe64369e
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-06 14:36:44 -0800

    Added scoring and editing mode.

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -7,8 +7,31 @@
 //
 
 import SpriteKit
+import GameplayKit // Allows randomness
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    // Labels to display the score and edit mode
+    var scoreLabel: SKLabelNode!
+    
+    var editLabel: SKLabelNode!
+    
+    // Property observers to make the labels update themselves when needed
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
     
     override func didMove(to view: SKView) {
         // Similar to UIImage, SKSpriteNode is a class that we use that can load any picture from the app bundle into our game. Then, we can modify that image's properties to our liking.
@@ -34,21 +57,59 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Assign the current scene to be the contact delegate. i.e. Tells us when contact occurs between two bodies.
         physicsWorld.contactDelegate = self
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+        
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // This is new to me and it's really cool. When a user touches the screen, the location of where the touch occurs will be stored in a constant named location.
         if let touch = touches.first {
             let location = touch.location(in:self)
-            let ball = SKSpriteNode(imageNamed: "ballRed")
-            // Then, we use SKPhysicsBody(circleOfRadius:) to give circular physics to the ball we're creating and restitution to give it a level of bounciness.
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-            // Set the collisionBitMask to be the contactBitMask so that we are notified about every collision.
-            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
-            ball.physicsBody!.restitution = 0.4
-            ball.position = location
-            ball.name = "ball"
-            addChild(ball)
+            
+            // Get a list of all the nodes that were tapped and check if it contains the editing label. This will tell us if we're in editing mode or not.
+            let objects = nodes(at: location)
+            
+            if objects.contains(editLabel) {
+                editingMode = !editingMode
+            } else {
+                // If we're in editing mode, we create boxes. If not, we create balls.
+                if editingMode {
+                    // Create a box with a random size and random color
+                    let size = CGSize(width: GKRandomDistribution(lowestValue: 16, highestValue: 128).nextInt(), height: 16)
+                    let box = SKSpriteNode(color: RandomColor(), size: size)
+                    
+                    // Give the box a random rotation and place it at the location that was touched on the screen.
+                    box.zRotation = RandomCGFloat(min: 0, max: 3)
+                    box.position = location
+                    
+                    box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                    box.physicsBody!.isDynamic = false
+                    
+                    addChild(box)
+                } else {
+                    let ball = SKSpriteNode(imageNamed: "ballRed")
+                    // We use SKPhysicsBody(circleOfRadius:) to give circular physics to the ball we're creating and restitution to give it a level of bounciness.
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                    
+                    // Set the collisionBitMask to be the contactBitMask so that we are notified about every collision.
+                    ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+                    ball.physicsBody!.restitution = 0.4
+                    
+                    ball.position = location
+                    ball.name = "ball"
+                    
+                    addChild(ball)
+                }
+            }
         }
     }
     
@@ -113,8 +174,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collisionBetween(ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
     
